f009867753e5ee06b929c7bf0c995046af86515c
Bercsényi István <programozo_1@kvlcomp.hu>
2020-10-30 08:06:51 +0100

    classstructureconstructors

---
+++ classstructureconstructors/src/main/java/ProductMain.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class ProductMain {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Product name?");
+        String name = scanner.nextLine();
+
+        System.out.println("Product price?");
+        int price = scanner.nextInt();
+
+        Product product = new Product(name, price);
+
+        System.out.println("Product datas:\nName: " + product.getName() + "\nPrice: " + product.getPrice());
+
+    }
+}

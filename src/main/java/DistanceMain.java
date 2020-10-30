edf8b5b975cb64a57719e7b70cf56dc0cffd9d6a
Bercsényi István <programozo_1@kvlcomp.hu>
2020-10-30 09:43:47 +0100

    localvariables

---
+++ src/main/java/DistanceMain.java
@@ -0,0 +1,10 @@
+public class DistanceMain {
+    public static void main(String[] args) {
+
+        Distance distance = new Distance(60.74, false);
+        System.out.println(distance.getDistance() + " " + distance.isPrecise());
+
+        int intPart = (int) distance.getDistance();
+        System.out.println(intPart);
+    }
+}
---
+++ src/main/java/LocalVariablesMain.java
@@ -0,0 +1,24 @@
+public class LocalVariablesMain {
+    public static void main(String[] args) {
+        boolean b;
+        System.out.println(b); //Nem megy
+        b = false;
+
+        int a = 2;
+        int i = 3, j = 4;
+        int k = i;
+
+        System.out.println(tmp);
+
+        String s = "Hello world";
+        String t = s;
+
+        {
+            int x = 0;
+            System.out.println(s);//Megy
+        }
+
+        System.out.println(x);  //Nem megy
+
+    }
+}

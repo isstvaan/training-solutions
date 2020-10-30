431544c312057ae34ee75c46a199b9fcf43f2713
Bercsényi István <programozo_1@kvlcomp.hu>
2020-10-30 11:42:21 +0100

    javaapi

---
+++ javaapi/src/main/java/Upper.java
@@ -0,0 +1,6 @@
+public class Upper {
+    public static void main(String[] args) {
+        String tmp = "Hello World";
+        System.out.println("Original: " + tmp + "\t Uppercase: " + tmp.toUpperCase());
+    }
+}

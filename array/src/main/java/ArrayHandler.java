79b0cab185a2bc540fae1a878f097ce2caee1d4a
Bercsényi István <programozo_1@kvlcomp.hu>
2020-11-02 10:55:43 +0100

    array

---
+++ array/src/main/java/ArrayHandler.java
@@ -0,0 +1,24 @@
+public class ArrayHandler {
+
+    private int find(int[] source, int itemToFind)
+    {
+        for (int i = 0; i < source.length; i++) {
+            if (itemToFind == source[i])
+            {
+                return  i;
+            }
+        }
+        return -1;
+    }
+
+    private boolean contains(int[] source, int itemToFind)
+    {
+        for (int i = 0; i < source.length; i++) {
+            if (itemToFind == source[i])
+            {
+                return  true;
+            }
+        }
+        return false;
+    }
+}

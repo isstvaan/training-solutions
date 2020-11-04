2771a5d3d45b9e3e261c50b29f34e314403360d8
Bercsényi István <programozo_1@kvlcomp.hu>
2020-11-04 09:40:33 +0100

    primitivetypes

---
+++ primitivetypes/src/main/java/PrimitiveTypes.java
@@ -0,0 +1,38 @@
+public class PrimitiveTypes {
+
+    public String toBinaryString(int n) {
+        String binStr = "";
+        int value = n;
+
+        //Komplementer számításhoz kell
+        if (value < 0) {
+            value++;
+        }
+        while (value != 0) {
+            binStr = (value % 2 != 0 ? "1" : "0") + binStr;
+            value = value / 2;
+        }
+
+        binStr = padLeft(binStr, 32, '0');
+
+        //Ha negeatív akkor a JAVA szerint vesszük a növelt érték egyes komplementerét
+        if (n < 0) {
+            String negativeBinStr = "";
+            for (int i = 0; i < binStr.length(); i++) {
+                negativeBinStr += binStr.charAt(i) == '0' ? '1' : '0';
+            }
+            binStr = negativeBinStr;
+        }
+
+        return binStr;
+    }
+
+    private String padLeft(String text, int length, char padChar) {
+        String paddedString = text;
+
+        for (int i = paddedString.length(); i < length; i++) {
+            paddedString = padChar + paddedString;
+        }
+        return paddedString;
+    }
+}
---
+++ week02/src/main/java/Phone.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+
+public class Phone {
+    private String type;
+    private int mem;
+
+    public Phone(String type, int mem) {
+        this.type = type;
+        this.mem = mem;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public int getMem() {
+        return mem;
+    }
+
+    public void setMem(int mem) {
+        this.mem = mem;
+    }
+
+    public static void main(String[] args) {
+        Phone phone1 = new Phone("iPhone", 256);
+        Phone phone2 = new Phone("iPhone W", 1024);
+
+        System.out.println("Phone1: type: " + phone1.getType() + " mem:" + phone1.getMem());
+        System.out.println("Phone2: type: " + phone2.getType() + " mem:" + phone2.getMem());
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Adja meg a telfon típusát:");
+        String type = scanner.nextLine();
+        System.out.println("Adja meg a telfon memóráját:");
+        int mem = scanner.nextInt();
+
+        Phone cunstomPhone = new Phone(type, mem);
+        System.out.println("Custom phone: type: " + cunstomPhone.getType() + " mem:" + cunstomPhone.getMem());
+
+        System.out.println(0.2+0.1);
+    }
+}

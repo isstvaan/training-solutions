21ed775202f36b7c055b6c3d595527ec292115dc
Bercsényi István <programozo_1@kvlcomp.hu>
2020-11-04 13:53:29 +0100

    typeconversion

---
+++ typeconversion/src/main/java/dataloss/DataLoss.java
@@ -0,0 +1,29 @@
+package dataloss;
+
+public class DataLoss {
+
+    public void dataLoss() {
+        long number = 0;
+        int counter = 0;
+
+        for (int i = 0; i < 3; ) {
+            float convertedNumberFloat = (float) number;
+            long convertedNumberLong = (long) convertedNumberFloat;
+
+            if (convertedNumberLong != number) {
+                System.out.println("Dataloss:");
+                System.out.println("Number: " + number);
+                System.out.println("Converted number: " + convertedNumberLong);
+                System.out.println("Number binary: " + Long.toString(number, 2));
+                System.out.println("Converted number binary: " + Long.toString(convertedNumberLong, 2));
+                System.out.println("Difference: " + (number - convertedNumberLong));
+                i++;
+            }
+            number++;
+        }
+    }
+
+    public static void main(String[] args) {
+        new DataLoss().dataLoss();
+    }
+}

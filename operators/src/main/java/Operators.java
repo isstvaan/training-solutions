1a670a61d5ec16843d248c46d5fd9321954bae7f
Bercsényi István <programozo_1@kvlcomp.hu>
2020-11-04 12:40:01 +0100

    operators

---
+++ operators/src/main/java/Operators.java
@@ -0,0 +1,10 @@
+public class Operators {
+
+    public boolean isEven(int number) {
+        return number % 2 == 0;
+    }
+
+    public int multiplyByPowerOfTwo(int number, int power) {
+        return number << power;
+    }
+}

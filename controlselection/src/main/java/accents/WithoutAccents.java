16ef92bd2ebfcde322deba1112ecbda03839d32d
Bercsényi István <programozo_1@kvlcomp.hu>
2020-11-09 08:42:46 +0100

    controlselection

---
+++ controlselection/src/main/java/accents/WithoutAccents.java
@@ -0,0 +1,22 @@
+package accents;
+
+public class WithoutAccents {
+
+    public char WithoutAccents(char withoutAccentsChar) {
+        switch (withoutAccentsChar) {
+            case 'ö':
+            case 'ő':
+                return 'o';
+            case 'ü':
+            case 'ű':
+            case 'ú':
+                return 'u';
+            case 'á':
+                return 'a';
+            case 'é':
+                return 'e';
+        }
+
+        return withoutAccentsChar;
+    }
+}

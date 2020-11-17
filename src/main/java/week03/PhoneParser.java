2f9322f013feb90d2594ab63b43fbde6f6e978f1
Bercsényi István <programozo_1@kvlcomp.hu>
2020-11-17 11:01:46 +0100

    week03.Operation

--- src/main/java/week03/PhoneParser.java
+++ src/main/java/week03/PhoneParser.java
@@ -3,18 +3,15 @@ package week03;
 import java.util.Scanner;
 
 public class PhoneParser {
-    public Phone parsePhone(String phoneNumber)
-    {
-        if (phoneNumber == null        )
-        {
+    public Phone parsePhone(String phoneNumber) {
+        if (phoneNumber == null) {
             throw new IllegalArgumentException("Phone number can't be null.");
         }
 
         String[] tmp = phoneNumber.split("\\+");
-        if (tmp.length < 2)
-        {
+        if (tmp.length < 2) {
             throw new IllegalArgumentException("Not valid phone number.");
         }
-        return new Phone(tmp[0],tmp[1]);
+        return new Phone(tmp[0], tmp[1]);
     }
 }

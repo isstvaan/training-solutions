e5aec7741ed314eaa0c3a7f9c06b78b584217454
Bercsényi István <programozo_1@kvlcomp.hu>
2020-11-19 14:13:09 +0100

    interfaces

---
+++ src/main/java/interfaces/simplethread/SimpleThread.java
@@ -0,0 +1,28 @@
+package interfaces.simplethread;
+
+import java.util.List;
+
+public class SimpleThread implements Runnable {
+    private List<String> tasks;
+
+    public SimpleThread(List<String> tasks) {
+        this.tasks = tasks;
+    }
+
+    public List<String> getTasks() {
+        return tasks;
+    }
+
+    private boolean nextStep() {
+        if (tasks.size() == 0) {
+            return false;
+        }
+        tasks.remove(tasks.size() - 1);
+        return tasks.size() > 0;
+    }
+
+    @Override
+    public void run() {
+        while (nextStep()) ;
+    }
+}

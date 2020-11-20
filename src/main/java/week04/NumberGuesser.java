2d9e9a8e7fbc8403dd73a041c50ebc8b1210e2a9
Bercsényi István <programozo_1@kvlcomp.hu>
2020-11-20 14:22:34 +0100

    NumberGuesser bonus

--- src/main/java/week04/NumberGuesser.java
+++ src/main/java/week04/NumberGuesser.java
@@ -4,21 +4,29 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class NumberGuesser {
+    private static final int ROUNDS = 7;
 
     public static void main(String[] args) {
         Random random = new Random();
         int number = 1 + random.nextInt(99);
-        System.out.println("Kitaláltam egy számot 1-100 között");
+        System.out.println("Kitaláltam egy számot 1-100 között, van rá " + ROUNDS + " köröd");
 
         Scanner scanner = new Scanner(System.in);
+        int counter = 0;
         while (true) {
-            System.out.println("Ird be az általad preferáld számot:");
+            counter++;
+            System.out.println(counter + " kör: Ird be az általad preferáld számot:");
             int userNumber = scanner.nextInt();
             if (userNumber == number) {
                 System.out.println("Gratulálok kitaláltad: " + number);
                 return;
             }
             System.out.println("Nem talált, az én számom ennél: " + (number > userNumber ? "nagyobb" : "kisebb"));
+
+            if (counter >= ROUNDS) {
+                System.out.println("Vesztettél, a helyes szám " + number + " lett volna");
+                return;
+            }
         }
     }
 }

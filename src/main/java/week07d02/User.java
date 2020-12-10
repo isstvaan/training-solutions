d7da2aae8075f84235650ee86d8b237e32b7263f
Steev <mechintos@gmail.com>
2020-12-10 19:42:22 +0100

    week07d02 interface

---
+++ src/main/java/week07d02/User.java
@@ -0,0 +1,17 @@
+package week07d02;
+
+public interface User {
+    String getUserName();
+
+    String getFirstName();
+
+    String getLastName();
+
+    default String getFullName() {
+        return getFirstName() + " " + getLastName() + " " + getUserName();
+    }
+
+    public static User of(String userName, String firstName, String lastName) {
+        return new UserImpl(userName, firstName, lastName);
+    }
+}

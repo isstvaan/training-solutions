d99a9280aafca1bb18b1fc317c5e3dfea279b198
Steev <mechintos@gmail.com>
2021-02-04 18:32:28 +0100

    week13d03 órai feladat, legtöbb irott oldal irojanak keresése

--- src/main/java/week13d03/AuthorMostPages.java
+++ src/main/java/week13d03/AuthorMostPages.java
@@ -3,8 +3,21 @@ package week13d03;
 import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
+import java.util.stream.Collectors;
 
 public class AuthorMostPages {
+    public String getThePostActiveAuthorStream(List<Book> books) {
+        return books
+                .stream()
+                .collect(Collectors.groupingBy(Book::getAuthor, Collectors.counting()))
+                .entrySet()
+                .stream()
+                .max((a, b) -> (int) (a.getValue() - b.getValue()))
+                .get()
+                .getKey();
+    }
+
+
     private Map.Entry<String, Integer> getMaxPageAuthor(Map<String, Integer> authors) {
         int maxPage = 0;
         Map.Entry<String, Integer> retVal = null;

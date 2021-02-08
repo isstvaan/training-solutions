41c3f17c3165a4f08804730086055e821b181c33
Steev <mechintos@gmail.com>
2021-02-08 19:14:42 +0100

    week14d05 órai feladat, hachiko.srt szó számláló

---
+++ src/main/java/week14d05/WordCounter.java
@@ -0,0 +1,31 @@
+package week14d05;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Arrays;
+import java.util.stream.Stream;
+
+public class WordCounter {
+
+    public long countName(String name, String filePath) {
+        try {
+            return Files.lines(Path.of(filePath), StandardCharsets.UTF_8)
+                    .filter(o -> o.toUpperCase().contains(name.toUpperCase()))
+                    .flatMap(WordCounter::lineToword)
+                    .filter(o -> o.toUpperCase().contains(name.toUpperCase()))
+                    .count();
+        } catch (IOException e) {
+            throw new IllegalStateException("File read error", e);
+        }
+    }
+
+    private static Stream<? extends String> lineToword(String line) {
+        return Arrays.stream(line.split(" "));
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new WordCounter().countName("Hach", "hachiko.srt"));
+    }
+}
